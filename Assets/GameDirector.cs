cad0d79960072da65b7d3c2e44cb9850560c4d4c
kayahiko777 <kayahiko777@gmail.com>
2024-08-17 10:48:49 +0900

    完成プロジェクト

---
+++ Assets/GameDirector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //UI‚ðŽg‚¤‚Ì‚Å–Y‚ê‚¸‚É’Ç‰Á
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject hpGauge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.hpGauge = GameObject.Find("hpGauge");
+    }
+
+    // Update is called once per frame
+    public void DecreaseHp()
+    {
+        this.hpGauge.GetComponent<Image>().fillAmount -= 0.1f;
+    }
+    void Update()
+    {
+        
+    }
+}
